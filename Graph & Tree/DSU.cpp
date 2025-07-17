5b933c8b5a4902413743b50be9ac964739cc5f9a
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-07-17 11:14:39 +0600

    Refactor DSU implementation for improved readability and structure

--- Graph & Tree/DSU.cpp
+++ Graph & Tree/DSU.cpp
@@ -1,36 +1,28 @@
-//Template for DSU
+struct DSU{
+    vector<int>parent, _size;
 
-vector<int>parent, _size; 
+    DSU(int n){
+        parent.resize(n);
+        iota(all(parent),0);
+        _size.assign(n,1); 
+    }
  
-int find(int i){ 
-    if(parent[i]==i) return i; 
-    return parent[i] = find(parent[i]); 
-} 
- 
-int size(int a){ 
-    a = find(a); 
-    return _size[a]; 
-} 
- 
-void Union(int a, int b){ 
-    a = find(a); 
-    b = find(b); 
-    if(a==b) return; 
-    if(_size[a]<_size[b]) swap(a,b); //union by size 
-    parent[b] = a; 
-    _size[a]+=_size[b]; 
-}
- 
-void solve(){
-    int n,k; 
-    cin>>n>>k; 
-    parent.resize(n+1); 
-    _size.assign(n+1,1); 
-    for(int i = 1; i<=n; i++) 
-        parent[i] = i; 
-    while(k--){ 
-        int u,v; 
-        cin>>u>>v; 
-        Union(u,v); 
+    int find(int i){ 
+        if(parent[i]==i) return i; 
+        return parent[i] = find(parent[i]); 
     } 
-}
+    
+    int size(int a){ 
+        a = find(a); 
+        return _size[a]; 
+    }
+    
+    void Union(int a, int b){ 
+        a = find(a); 
+        b = find(b); 
+        if(a==b) return; 
+        if(_size[a]<_size[b]) swap(a,b);
+        parent[b] = a; 
+        _size[a]+=_size[b]; 
+    }
+};
