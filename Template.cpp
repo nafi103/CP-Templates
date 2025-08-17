8b5eaeb8dbd9f41d63a999e339f939b500cbeda2
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-08-17 11:12:18 +0600

    Removed unused templates

--- Template.cpp
+++ Template.cpp
@@ -12,22 +12,13 @@ using namespace __gnu_pbds;
 #define pi acos(-1.0)
 const int mod = 998244353;
 #define inf 1e18+10
-#define pb push_back
-#define ff first
-#define ss second
 #define sz(x) (int)(x).size()
 #define LSOne(x) ((x)&(-x))
 #define all(x) x.begin(), x.end()
 #define readv(v)      \
     for (auto &x : v) \
     cin >> x
-#define writev(v)     \
-    for (auto &x : v) \
-    cout << x << " "; \
-    cout<<endl
 #define endl "\n"
-#define yes cout<<"YES"<<endl
-#define no cout<<"NO"<<endl
 #define remove_punctuation(text) regex_replace(text, regex(R"([^\w\s])"), "")
 #define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
 template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
