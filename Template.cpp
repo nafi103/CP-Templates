406359c567ccec71e284441a9d83538ea394e6d7
Nasif Shahrier Nafi <nasifshahrierrifat@gmail.com>
2025-11-06 23:19:09 +0600

    Updated Template and seperated PBDS

--- Template.cpp
+++ Template.cpp
@@ -1,52 +1,46 @@
-#include<bits/stdc++.h>
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
 
 using namespace std;
-using namespace chrono;
-using namespace __gnu_pbds;
 
 /****************************************************************/
 
 #define int long long
 #define pi acos(-1.0)
 const int mod = 998244353;
-#define inf 1e18+10
+#define inf 1e18 + 10
 #define sz(x) (int)(x).size()
-#define LSOne(x) ((x)&(-x))
+#define LSOne(x) ((x) & (-x))
 #define all(x) x.begin(), x.end()
-#define readv(v)      \
-    for (auto &x : v) \
-    cin >> x
 #define endl "\n"
-#define fastIO ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr)
-template <class T> using pbds = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update >;
 
 #ifndef ONLINE_JUDGE
 #include "debug.h"
-#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#define debug(x)                                        \
+    cerr << "Line " << __LINE__ << ": " << #x << " = "; \
+    _print(x);                                          \
+    cerr << endl;
 #else
 #define debug(...)
 #endif
 
 /****************************************************************/
 
-
 void solve()
 {
-    
+
 }
 
 int32_t main()
 {
     // freopen("paint.in", "r", stdin);
     // freopen("paint.out", "w", stdout);
-    fastIO;
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
     cout.precision(10);
     cout.setf(ios::fixed);
     int t = 1;
     cin >> t;
-    for(int z = 1; z<=t; z++){
+    for (int z = 1; z <= t; z++)
+    {
         // cout<<"Case "<<z<<": ";
         solve();
     }
